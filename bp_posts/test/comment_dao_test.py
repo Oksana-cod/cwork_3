69998c975cb480d1a3a99e90d16db0a97b6e5f7d
Oksana-cod <105298575+Oksana-cod@users.noreply.github.com>
2022-06-26 20:12:21 +0500

    Update comment_dao_test.py

--- bp_posts/test/comment_dao_test.py
+++ bp_posts/test/comment_dao_test.py
@@ -1,5 +1,14 @@
 
+from bp_posts.views import CommentDAO
+
+
 class TestCommentsDAO: #Тест на комментарии
 
     def test_foo(self):
-        pass
+        comment_dao_instance = CommentDAO("./bp_posts/test/comment_mock.json")
+        return comment_dao_instance
+
+        assert type(CommentDAO()._load_comments()) == list
+        
+        
+
